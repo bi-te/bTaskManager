0abd17508c34bd63c12c9ec5aa035b673e7f6971
Obolonsky Denis <denis.obolonsky@gmail.com>
2020-11-16 19:42:41 +0200

    added try-with-resources

--- src/main/java/ua/edu/sumdu/j2se/obolonsky/tasks/TaskIO.java
+++ src/main/java/ua/edu/sumdu/j2se/obolonsky/tasks/TaskIO.java
@@ -15,156 +15,149 @@ public class TaskIO {
     // Binary
 
     public static void write(@NotNull AbstractTaskList tasks, @NotNull OutputStream out) throws IOException {
-        DataOutputStream outStream = new DataOutputStream(out);
-        outStream.writeInt(tasks.size());
-        for (Task task : tasks) {
-            outStream.writeInt(task.getTitle().length());
-            outStream.writeChars(task.getTitle());
-            outStream.writeBoolean(task.isActive());
-            int interval = task.getRepeatInterval();
-            outStream.writeInt(interval);
-            if (interval > 0) {
-                outStream.writeLong(task.getStartTime().toEpochSecond(ZoneOffset.UTC));
-                outStream.writeLong(task.getEndTime().toEpochSecond(ZoneOffset.UTC));
-            } else {
-                outStream.writeLong(task.getTime().toEpochSecond(ZoneOffset.UTC));
+        try (DataOutputStream outStream = new DataOutputStream(out)) {
+            outStream.writeInt(tasks.size());
+            for (Task task : tasks) {
+                outStream.writeInt(task.getTitle().length());
+                outStream.writeChars(task.getTitle());
+                outStream.writeBoolean(task.isActive());
+                int interval = task.getRepeatInterval();
+                outStream.writeInt(interval);
+                if (interval > 0) {
+                    outStream.writeLong(task.getStartTime().toEpochSecond(ZoneOffset.UTC));
+                    outStream.writeLong(task.getEndTime().toEpochSecond(ZoneOffset.UTC));
+                } else {
+                    outStream.writeLong(task.getTime().toEpochSecond(ZoneOffset.UTC));
+                }
             }
         }
-        outStream.close();
     }
 
     public static void read(@NotNull AbstractTaskList tasks, @NotNull InputStream in) throws IOException {
-        DataInputStream inStream = new DataInputStream(in);
-        int size = inStream.readInt();
-        Task task;
-        LocalDateTime start;
-        LocalDateTime end;
-        for (; size > 0; size--) {
-            int length = inStream.readInt() * 2;
-            byte[] b = new byte[length];
-            inStream.readFully(b, 0, length);
-            String title = new String(b, 0, length, StandardCharsets.UTF_16);
-            boolean active = inStream.readBoolean();
-            int interval = inStream.readInt();
-            start = LocalDateTime.ofEpochSecond(inStream.readLong(), 0, ZoneOffset.UTC);
+        try (DataInputStream inStream = new DataInputStream(in)) {
+            int size = inStream.readInt();
+            Task task;
+            LocalDateTime start;
+            LocalDateTime end;
+            for (; size > 0; size--) {
+                int length = inStream.readInt() * 2;
+                byte[] b = new byte[length];
+                inStream.readFully(b, 0, length);
+                String title = new String(b, 0, length, StandardCharsets.UTF_16);
+                boolean active = inStream.readBoolean();
+                int interval = inStream.readInt();
+                start = LocalDateTime.ofEpochSecond(inStream.readLong(), 0, ZoneOffset.UTC);
 
-            if (interval > 0) {
-                end = LocalDateTime.ofEpochSecond(inStream.readLong(), 0, ZoneOffset.UTC);
-                task = new Task(title, start, end, interval);
-            } else {
-                task = new Task(title, start);
+                if (interval > 0) {
+                    end = LocalDateTime.ofEpochSecond(inStream.readLong(), 0, ZoneOffset.UTC);
+                    task = new Task(title, start, end, interval);
+                } else {
+                    task = new Task(title, start);
+                }
+                task.setActive(active);
+                tasks.add(task);
             }
-            task.setActive(active);
-            tasks.add(task);
         }
-        inStream.close();
     }
 
     public static void writeBinary(@NotNull AbstractTaskList tasks, @NotNull File file) throws IOException {
-        FileOutputStream fo = new FileOutputStream(file);
-        DataOutputStream outStream = new DataOutputStream(fo);
-        outStream.writeInt(tasks.size());
-        for (Task task : tasks) {
-            outStream.writeInt(task.getTitle().length());
-            outStream.writeChars(task.getTitle());
-            outStream.writeBoolean(task.isActive());
-            int interval = task.getRepeatInterval();
-            outStream.writeInt(interval);
-            if (interval > 0) {
-                outStream.writeLong(task.getStartTime().toEpochSecond(ZoneOffset.UTC));
-                outStream.writeLong(task.getEndTime().toEpochSecond(ZoneOffset.UTC));
-            } else {
-                outStream.writeLong(task.getTime().toEpochSecond(ZoneOffset.UTC));
+        try (FileOutputStream fo = new FileOutputStream(file);
+             DataOutputStream outStream = new DataOutputStream(fo)) {
+
+            outStream.writeInt(tasks.size());
+            for (Task task : tasks) {
+                outStream.writeInt(task.getTitle().length());
+                outStream.writeChars(task.getTitle());
+                outStream.writeBoolean(task.isActive());
+                int interval = task.getRepeatInterval();
+                outStream.writeInt(interval);
+                if (interval > 0) {
+                    outStream.writeLong(task.getStartTime().toEpochSecond(ZoneOffset.UTC));
+                    outStream.writeLong(task.getEndTime().toEpochSecond(ZoneOffset.UTC));
+                } else {
+                    outStream.writeLong(task.getTime().toEpochSecond(ZoneOffset.UTC));
+                }
             }
         }
-        outStream.close();
-        fo.close();
     }
 
     public static void readBinary(@NotNull AbstractTaskList tasks, @NotNull File file) throws IOException {
-        FileInputStream fi = new FileInputStream(file);
-        DataInputStream inStream = new DataInputStream(fi);
-        int size = inStream.readInt();
-        Task task;
-        LocalDateTime start;
-        LocalDateTime end;
-        byte[] b;
-        for (; size > 0; size--) {
-            int length = inStream.readInt() * 2;
-            b = new byte[length];
-            inStream.readFully(b, 0, length);
-            String title = new String(b, 0, length, StandardCharsets.UTF_16);
-            boolean active = inStream.readBoolean();
-            int interval = inStream.readInt();
-            start = LocalDateTime.ofEpochSecond(inStream.readLong(), 0, ZoneOffset.UTC);
+        try (FileInputStream fi = new FileInputStream(file);
+             DataInputStream inStream = new DataInputStream(fi)) {
 
-            if (interval > 0) {
-                end = LocalDateTime.ofEpochSecond(inStream.readLong(), 0, ZoneOffset.UTC);
-                task = new Task(title, start, end, interval);
-            } else {
-                task = new Task(title, start);
+            int size = inStream.readInt();
+            Task task;
+            LocalDateTime start;
+            LocalDateTime end;
+            for (; size > 0; size--) {
+                int length = inStream.readInt() * 2;
+                byte[] b = new byte[length];
+                inStream.readFully(b, 0, length);
+                String title = new String(b, 0, length, StandardCharsets.UTF_16);
+                boolean active = inStream.readBoolean();
+                int interval = inStream.readInt();
+                start = LocalDateTime.ofEpochSecond(inStream.readLong(), 0, ZoneOffset.UTC);
+
+                if (interval > 0) {
+                    end = LocalDateTime.ofEpochSecond(inStream.readLong(), 0, ZoneOffset.UTC);
+                    task = new Task(title, start, end, interval);
+                } else {
+                    task = new Task(title, start);
+                }
+                task.setActive(active);
+                tasks.add(task);
             }
-            task.setActive(active);
-            tasks.add(task);
         }
-        inStream.close();
-        fi.close();
     }
 
     //Text
 
     public static void write(@NotNull AbstractTaskList tasks, @NotNull Writer out) throws IOException {
         Gson gson = createGson();
-        JsonWriter jsonWriter = gson.newJsonWriter(out);
-
-        jsonWriter.beginArray();
-        for (Task task : tasks) {
-            jsonWriter.jsonValue(gson.toJson(task));
+        try (JsonWriter jsonWriter = gson.newJsonWriter(out)) {
+            jsonWriter.beginArray();
+            for (Task task : tasks) {
+                jsonWriter.jsonValue(gson.toJson(task));
+            }
+            jsonWriter.endArray();
         }
-        jsonWriter.endArray();
-        out.flush();
     }
 
     public static void read(@NotNull AbstractTaskList tasks, @NotNull Reader in) throws IOException {
         Gson gson = createGson();
-        JsonReader jsonReader = gson.newJsonReader(in);
-
-        jsonReader.beginArray();
-        while (jsonReader.hasNext()) {
-            tasks.add(gson.fromJson(jsonReader, Task.class));
+        try (JsonReader jsonReader = gson.newJsonReader(in)) {
+            jsonReader.beginArray();
+            while (jsonReader.hasNext()) {
+                tasks.add(gson.fromJson(jsonReader, Task.class));
+            }
+            jsonReader.endArray();
         }
-        jsonReader.endArray();
 
     }
 
     public static void writeText(@NotNull AbstractTaskList tasks, @NotNull File file) throws IOException {
         Gson gson = createGson();
-        FileWriter fw = new FileWriter(file);
-        JsonWriter jsonWriter = gson.newJsonWriter(fw);
+        try (FileWriter fw = new FileWriter(file);
+             JsonWriter jsonWriter = gson.newJsonWriter(fw)) {
 
-        jsonWriter.beginArray();
-        for (Task task : tasks) {
-            jsonWriter.jsonValue(gson.toJson(task));
+            jsonWriter.beginArray();
+            for (Task task : tasks) {
+                jsonWriter.jsonValue(gson.toJson(task));
+            }
+            jsonWriter.endArray();
         }
-        jsonWriter.endArray();
-        fw.flush();
-        fw.close();
-
     }
 
     public static void readText(@NotNull AbstractTaskList tasks, @NotNull File file) throws IOException {
         Gson gson = createGson();
-        FileReader fr = new FileReader(file);
-        JsonReader jsonReader = gson.newJsonReader(fr);
-
-        jsonReader.beginArray();
-        while (jsonReader.hasNext()) {
-            tasks.add(gson.fromJson(jsonReader, Task.class));
+        try (FileReader fr = new FileReader(file);
+             JsonReader jsonReader = gson.newJsonReader(fr)) {
+            jsonReader.beginArray();
+            while (jsonReader.hasNext()) {
+                tasks.add(gson.fromJson(jsonReader, Task.class));
+            }
+            jsonReader.endArray();
         }
-        jsonReader.endArray();
-        fr.close();
-
-
     }
 
     private static Gson createGson() {
