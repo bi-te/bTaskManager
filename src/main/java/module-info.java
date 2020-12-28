f6fa08efc5c7128186ff41263fd7801ac2d00d0f
Obolonsky Denis <denis.obolonsky@gmail.com>
2020-12-28 17:24:16 +0200

    module settings

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,7 +7,7 @@ module ua.edu.sumdu.j2se.obolonsky {
 
     opens ua.edu.sumdu.j2se.obolonsky.app to javafx.fxml;
     opens ua.edu.sumdu.j2se.obolonsky.tasks to com.google.gson;
-    
+
     exports ua.edu.sumdu.j2se.obolonsky.tasks;
     exports ua.edu.sumdu.j2se.obolonsky.app;
 
